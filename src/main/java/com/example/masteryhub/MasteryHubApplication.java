fbeb20d014bf1abddcce1fe8bc18153fe7db44d3
rchelk <chethmi.rathnayake@ifs.com>
2025-04-02 09:13:14 +0530

    Initial commit

---
+++ src/main/java/com/example/masteryhub/MasteryHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.masteryhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasteryHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasteryHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/masteryhub/MasteryHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.masteryhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasteryHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
