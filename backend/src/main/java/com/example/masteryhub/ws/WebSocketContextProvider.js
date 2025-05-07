6746a98f19cb62f50aecefa7820b3afa45b6c8a7
Saliya Pathum <charukabandara2002@gmail.com>
2025-05-07 19:33:52 +0530

    useWebSocket and update

---
+++ backend/src/main/java/com/example/masteryhub/ws/WebSocketContextProvider.js
@@ -0,0 +1,31 @@
+import { CompatClient, Stomp } from "@stomp/stompjs";
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+
+const WsContext = createContext<CompatClient | null>(null);
+
+export const useWebSocket = () => useContext(WsContext);
+
+export const WebSocketContextProvider = () => {
+  const [stompClient, setStompClient] = useState<CompatClient | null>(null);
+
+  useEffect(() => {
+    const client = Stomp.client(`${import.meta.env.VITE_API_URL}/ws`);
+
+    client.connect(
+      {},
+      () => {
+        console.log("Connected to WebSocket");
+        setStompClient(client);
+      },
+     
+    );
+
+    return () => {
+      if (client.connected) {
+        client.disconnect(() => console.log("Disconnected from WebSocket"));
+      }
+    };
+  }, []);
+
+  return <WsContext.Provider value={stompClient}>{children}</WsContext.Provider>;
+};
